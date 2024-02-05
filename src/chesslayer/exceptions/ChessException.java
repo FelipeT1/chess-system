ea025f04299cb6cc2823097bc0a1725b02632133
FelipeT <felipeteixeiramello99@gmail.com>
2024-02-05 05:27:16 -0300

    Made ChessException BE a BoardException

--- src/chesslayer/exceptions/ChessException.java
+++ src/chesslayer/exceptions/ChessException.java
@@ -1,6 +1,10 @@
 package chesslayer.exceptions;
 
-public class ChessException extends RuntimeException{
+import boardlayer.exceptions.BoardException;
+
+
+// Uma excessão de xadrez é uma de tabuleiro
+public class ChessException extends BoardException {
     public ChessException(String message) {
         super(message);
     }
