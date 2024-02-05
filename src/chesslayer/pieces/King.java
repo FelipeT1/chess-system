1e52243dfeca18f3f1e403e919e92abf47f24247
FelipeT <felipeteixeiramello99@gmail.com>
2024-02-05 10:17:38 -0300

    Implemented King movements

--- src/chesslayer/pieces/King.java
+++ src/chesslayer/pieces/King.java
@@ -1,6 +1,7 @@
 package chesslayer.pieces;
 
 import boardlayer.Board;
+import boardlayer.Position;
 import chesslayer.ChessPiece;
 import chesslayer.enums.Color;
 
@@ -12,7 +13,52 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+        // cima
+        p.setValues(position.getRow()-1, position.getCol());
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // baixo
+        p.setValues(position.getRow()+1, position.getCol());
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // esquerda
+        p.setValues(position.getRow(), position.getCol()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // direita
+        p.setValues(position.getRow(), position.getCol()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // diagonal esquerda cima
+        p.setValues(position.getRow()-1, position.getCol()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // diagonal esquerda baixo
+        p.setValues(position.getRow()+1, position.getCol()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // diagonal direita cima
+        p.setValues(position.getRow()-1, position.getCol()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
+        // diagonal direita baixo
+        p.setValues(position.getRow()+1, position.getCol()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            moves[p.getRow()][p.getCol()] = true;
+        }
         return moves;
+    }
+
+    private boolean canMove(Position p) {
+        return !getBoard().thereIsAPiece(p) || isThereOpponentPiece(p);
     }
 
     @Override
